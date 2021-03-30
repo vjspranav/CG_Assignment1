597df6097d380eae030988eae365da04e27c7c24
vjspranav <pranavasri@live.in>
2021-03-31 02:01:27 +0530

    Hey Players

---
+++ source/coordinate.cpp
@@ -0,0 +1,7 @@
+#include <coordinate.h>
+#include <main.h>
+
+Coordinate::Coordinate(int x, int y){
+    this->x = x;
+    this->y = y;
+}
---
+++ source/coordinate.h
@@ -0,0 +1,15 @@
+//#include "main.h"
+
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+
+class Coordinate {
+
+public:
+    int x, y;
+    Coordinate() {}
+    Coordinate(int x, int y);
+};
+
+#endif // COORDINATE_H
